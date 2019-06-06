ddac11fb9d5d978662e542dd5c455fee50c3b246
John Zhang <798105509@qq.com>
2019-06-06 15:43:02 +0800

    added mvb

--- torchreid/data/datasets/image/market1501-bak.py
+++ torchreid/data/datasets/image/market1501-bak.py
@@ -8,7 +8,6 @@ import os.path as osp
 import glob
 import re
 import warnings
-import csv
 
 from torchreid.data.datasets import ImageDataset
 
@@ -32,11 +31,11 @@ class Market1501(ImageDataset):
     def __init__(self, root='', market1501_500k=False, **kwargs):
         self.root = osp.abspath(osp.expanduser(root))
         self.dataset_dir = osp.join(self.root, self.dataset_dir)
-        # self.download_dataset(self.dataset_dir, self.dataset_url)
-    
+        self.download_dataset(self.dataset_dir, self.dataset_url)
+        
         # allow alternative directory structure
         self.data_dir = self.dataset_dir
-        data_dir = osp.join(self.data_dir, 'Image')
+        data_dir = osp.join(self.data_dir, 'Market-1501-v15.09.15')
         if osp.isdir(data_dir):
             self.data_dir = data_dir
         else:
@@ -44,10 +43,10 @@ class Market1501(ImageDataset):
                           'put data folders such as "bounding_box_train" under '
                           '"Market-1501-v15.09.15".')
         
-        self.train_dir = osp.join(self.data_dir, 'Info/sa_train.csv')
-        self.query_dir = osp.join(self.data_dir, 'Info/sa_query.csv')
-        self.gallery_dir = osp.join(self.data_dir, 'Info/sa_gallery.csv')
-        # self.extra_gallery_dir = osp.join(self.data_dir, 'images')
+        self.train_dir = osp.join(self.data_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.data_dir, 'query')
+        self.gallery_dir = osp.join(self.data_dir, 'bounding_box_test')
+        self.extra_gallery_dir = osp.join(self.data_dir, 'images')
         self.market1501_500k = market1501_500k
 
         required_files = [
@@ -55,10 +54,10 @@ class Market1501(ImageDataset):
             self.train_dir,
             self.query_dir,
             self.gallery_dir
-        ] 
-        # if self.market1501_500k:
-        #     required_files.append(self.extra_gallery_dir)
-        # self.check_before_run(required_files)
+        ]
+        if self.market1501_500k:
+            required_files.append(self.extra_gallery_dir)
+        self.check_before_run(required_files)
 
         train = self.process_dir(self.train_dir, relabel=True)
         query = self.process_dir(self.query_dir, relabel=False)
@@ -69,12 +68,27 @@ class Market1501(ImageDataset):
         super(Market1501, self).__init__(train, query, gallery, **kwargs)
 
     def process_dir(self, dir_path, relabel=False):
-        ret = []
-        # pids = set()
-        with open(osp.join(dir_path)) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                filename,pid,cam = row[0],row[1],row[2]
-                # pids.add(pid)
-                ret.append((filename,pid,cam))
-        return ret
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        pid_container = set()
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1:
+                continue # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid:label for label, pid in enumerate(pid_container)}
+
+        data = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1:
+                continue # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1 # index starts from 0
+            if relabel:
+                pid = pid2label[pid]
+            data.append((img_path, pid, camid))
+
+        return data
